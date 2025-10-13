e999dec89076a87c0eb2c2b98d2b41b7ed0c964f
jahorta <jahorta00000@gmail.com>
2025-10-13 07:06:51 -0500

    more name changes

--- SimCore/Utils/EnsureSys.h
+++ SimCore/Utils/EnsureSys.h
@@ -2,6 +2,6 @@
 #include <string>
 
 namespace simcore {
-	// Returns true if <exe_dir>\Sys exists (and contains dsp_coef.bin), else copies from qt_base\Sys.
-	bool EnsureSysBesideExe(const std::string& qt_base_dir);
+	// Returns true if <exe_dir>\Sys exists (and contains dsp_coef.bin), else copies from dolphin_base_dir\Sys.
+	bool EnsureSysBesideExe(const std::string& dolphin_base_dir);
 }
