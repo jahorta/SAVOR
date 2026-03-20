993dd5adbaca752896ff799e0043828f834dbb1b
jahorta <jahorta00000@gmail.com>
2026-03-20 08:27:25 -0500

    Initialize DB service in SoaSimQt startup

--- SoaSimQt/main.cpp
+++ SoaSimQt/main.cpp
@@ -1,12 +1,39 @@
 #include "MainWindow.h"
+#include "DB/DBCore/DbService.h"
+
 #include <QtCore/QCoreApplication>
+#include <QtCore/QSettings>
 #include <QtWidgets/QApplication>
+
+namespace {
+constexpr auto kSettingsGroup = "Settings";
+constexpr auto kDbRootKey = "db_root";
+
+void initializeDatabase()
+{
+    QSettings settings;
+    settings.beginGroup(kSettingsGroup);
+    const QString dbRoot = settings.value(kDbRootKey).toString().trimmed();
+    settings.endGroup();
+
+    if (!dbRoot.isEmpty()) {
+        simcore::db::DBService::instance().set_database_root(dbRoot.toStdString());
+    }
+
+    simcore::db::DBService::instance().start();
+}
+}
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
     QCoreApplication::setOrganizationName("OpenAI");
     QCoreApplication::setApplicationName("SoaSimQt");
+
+    initializeDatabase();
+    QObject::connect(&app, &QCoreApplication::aboutToQuit, []() {
+        simcore::db::DBService::instance().stop();
+    });
 
     MainWindow window;
     window.show();
