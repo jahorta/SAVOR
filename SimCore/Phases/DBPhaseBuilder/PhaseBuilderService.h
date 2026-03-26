0ed39e9ed8cd45b85ccc2d9f518783ca49ff97a4
jahorta <jahorta00000@gmail.com>
2026-03-26 08:15:40 -0500

    Remove PhaseBuilderService submission API

--- SimCore/Phases/DBPhaseBuilder/PhaseBuilderService.h
+++ SimCore/Phases/DBPhaseBuilder/PhaseBuilderService.h
@@ -1,7 +1,6 @@
 #pragma once
 #include "../../Utils/IniDoc.h"
 #include "../../DB/ProgramKindsRepo.h"
-#include "../../DB/ProgramDB/IProgramDBCodec.h"
 #include "../../DB/DBCore/ObjectStore.h"
 #include "../../DB/SeedProbeRepo.h"
 #include "../../DB/DeltaSeedRepo.h"
@@ -10,22 +9,14 @@
 #include "../../DB/ExplorerSettingsRepo.h"
 #include "../../DB/ExplorerSettingsPlanLinkRepo.h"
 #include "../../DB/ExplorerSettingsPredicateRepo.h"
-#include "../../DB/Scheduling/JobSetsRepo.h"
 #include "PhaseBuilderPreview.h"
 #include "PhaseBuilderSchemas.h"
 #include <string>
 #include <vector>
-#include <optional>
-#include <cstdint>
 
 namespace simcore::db::phasebuilder {
 
     struct ValidationError { std::string field; std::string message; };
-
-    struct SubmitResult {
-        int64_t job_set_id{};
-        int program_kind{};
-    };
 
     class PhaseBuilderService {
     public:
@@ -34,14 +25,6 @@ namespace simcore::db::phasebuilder {
         static std::vector<ValidationError> Validate(int program_kind, const IniDoc& ini);
 
         static DbResult<PhasePreview> Preview(int program_kind, const IniDoc& ini);
-
-        static DbResult<SubmitResult> Submit(
-            const std::string& purpose,
-            int program_kind,
-            const IniDoc& ini,
-            std::optional<std::string> meta_text = {},
-            std::optional<int64_t> expected_total_override = {}
-        );
 
     private:
         static DbResult<TasMoviePreview>    PreviewTas(const IniDoc& ini);
