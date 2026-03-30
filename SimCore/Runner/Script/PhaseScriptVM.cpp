4df39291c81bc49efc0d6f36e28fac9560abca6d
jahorta <jahorta00000@gmail.com>
2026-03-30 09:44:50 -0500

    added tags to input

--- SimCore/Runner/Script/PhaseScriptVM.cpp
+++ SimCore/Runner/Script/PhaseScriptVM.cpp
@@ -302,7 +302,7 @@ namespace simcore {
             wait_for_visual_debug_gate();
             GCInputFrame f{}; std::memcpy(&f, frames + (idx * sizeof(GCInputFrame)), sizeof(GCInputFrame)); applied_plan.push_back(f);
             const uint32_t vi_before = static_cast<uint32_t>(host_.getViFieldCountApprox() & 0xFFFFFFFFull);
-            SCLOGDX("[vm] setting input [%d]: %s", idx, DescribeFrameCompact(f).c_str());
+            SCLOGDX(SC_TAGS("VM","input"), "setting input [%d]: %s", idx, DescribeFrameCompact(f).c_str());
             host_.setInput(f);
             host_.stepOneFrameBlocking();
             const uint32_t vi_after = static_cast<uint32_t>(host_.getViFieldCountApprox() & 0xFFFFFFFFull);
