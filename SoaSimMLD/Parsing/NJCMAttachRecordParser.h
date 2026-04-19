fcd226a26643d675aefa2c72b1a138d449f628da
jahorta <jahorta00000@gmail.com>
2026-04-18 19:00:10 -0500

    Remove legacy NJCM/NJTL parsing module source files

--- SoaSimMLD/Parsing/NJCMAttachRecordParser.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "NJCMDecodeContext.h"
-
-namespace soasim::mld::parsing {
-
-[[nodiscard]] std::optional<model::NjAttachRecord> parseAttachRecord(const NjcmDecodeContext& ctx, std::size_t attachOffset);
-
-} // namespace soasim::mld::parsing
